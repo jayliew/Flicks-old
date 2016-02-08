5b9f9af40f33e53bca7fd1630cf523db6fbc7e1f
Jay Liew <jayliew@jayliew.com>
2016-02-07 19:22:41 -0800

    added progress HUD

--- Flicks/MoviesViewController.swift
+++ Flicks/MoviesViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import AFNetworking
+import MBProgressHUD
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
@@ -23,33 +24,48 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         tableView.dataSource = self
         tableView.delegate = self
         
+        
+        loadDataFromNetwork()
+    }
+
+    func loadDataFromNetwork() {
+        
+        // ... Create the NSURLRequest (myRequest) ...
         let clientId = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
         let url = NSURL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=\(clientId)")
         let request = NSURLRequest(URL: url!)
+
+        // Configure session so that completion handler is executed on main UI thread
         let session = NSURLSession(
             configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
             delegate:nil,
             delegateQueue:NSOperationQueue.mainQueue()
         )
         
+        // Display HUD right before the request is made
+        MBProgressHUD.showHUDAddedTo(self.view, animated: true)
+        
         let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
             completionHandler: { (dataOrNil, responseOrNil, errorOrNil) in
+                // Hide HUD once the network request comes back (must be done on main UI thread)
+                MBProgressHUD.hideHUDForView(self.view, animated: true)
+
                 if let requestError = errorOrNil {
-//                    errorCallback?(requestError)
+                    //                    errorCallback?(requestError)
                 } else {
                     if let data = dataOrNil {
                         if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
                             data, options:[]) as? NSDictionary {
-//                                NSLog("response: \(responseDictionary)")
+                                //                                NSLog("response: \(responseDictionary)")
                                 self.movies = responseDictionary["results"] as? [NSDictionary]
-//                                successCallback(responseDictionary)
+                                //                                successCallback(responseDictionary)
                         }
                     }
                 }
         });
         task.resume()
     }
-
+    
     // MARK: Navigation
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
@@ -68,7 +84,8 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         vc.titleLabel.text = title
         vc.overviewTextView.text = overview
     }
-
+    
+    // MARK: UITableViewDataSource
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("com.jayliew.MovieViewCell", forIndexPath: indexPath) as! MovieViewCell
@@ -87,8 +104,6 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
         return cell
     }
-    
-    // MARK: UITableViewDataSource
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         print(movies)
